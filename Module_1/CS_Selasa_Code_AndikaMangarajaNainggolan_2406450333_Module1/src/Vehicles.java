dcfc937945f4a202d45bbebbf214c61f48b8c0cf
aalalain <ashgreninja270@gmail.com>
2025-09-09 12:04:53 +0700

    PreCS1 - Create a repository

--- Module_1/CS_Selasa_Code_AndikaMangarajaNainggolan_2406450333_Module1/src/Vehicles.java
+++ Module_1/CS_Selasa_Code_AndikaMangarajaNainggolan_2406450333_Module1/src/Vehicles.java
@@ -1,2 +1,38 @@
 public class Vehicles {
+
+    private String brand;
+    private double payRate;
+    private final int EMPLOYEE_ID;
+
+    private static int nextID = 1000;
+
+    public static final double STARTING_PAY_RATE = 7.75;
+
+    public Employee(String brand) {
+        this.brand = brand;
+        EMPLOYEE_ID = getNextID();
+        payRate = STARTING_PAY_RATE;
+    }
+
+    public Employee(String name, double startingPay) {
+        this.brand = name;
+        EMPLOYEE_ID = getNextID();
+        payRate = startingPay;
+    }
+
+    public String getName() { return brand; }
+
+    public int getEmployeeID() { return EMPLOYEE_ID; }
+
+    public double getPayRate() { return payRate; }
+
+    public void changeName(String newName) { brand = newName; }
+
+    public void changePayRate(double newRate) { payRate = newRate; }
+
+    public static int getNextID() {
+        int id = nextID;
+        nextID++;
+        return id;
+    }
 }

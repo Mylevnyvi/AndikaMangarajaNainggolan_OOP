366c6af068545b1cfbc43aab7f809e638c61b51d
aalalain <ashgreninja270@gmail.com>
2025-11-21 11:05:14 +0700

    Frontend Changes CS 10

--- Frontend_AndikaMangarajaNainggolan/core/src/main/java/com/andika/frontend/obstacles/BaseObstacle.java
+++ Frontend_AndikaMangarajaNainggolan/core/src/main/java/com/andika/frontend/obstacles/BaseObstacle.java
@@ -1,7 +1,6 @@
 package com.andika.frontend.obstacles;
 
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-<<<<<<< HEAD
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
@@ -61,44 +60,3 @@ public abstract class BaseObstacle {
 
     protected abstract float getRenderWidth(); // Abstract method for specific render width
 }
-=======
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.Rectangle;
-
-public abstract class BaseObstacle {
-    private Vector2 position;
-    private Rectangle collider;
-    private float length;
-    private final float width = 10f;
-    Boolean active = false;
-
-    public BaseObstacle(Vector2 startPosition, int length){
-        position = new Vector2(startPosition);
-        collider = new Rectangle(position.x, position.y, width, length);
-    }
-    private void updateCollider() {
-        collider.set(position.x, position.y, width, length);
-    }
-
-    public void initialize(Vector2 startPosition) {
-        position = new Vector2(0, 0);
-        updateCollider();
-    }
-
-    public boolean isColliding(Rectangle playerCollider) {
-        return collider.overlaps(playerCollider);
-    }
-
-
-    public void renderShape(ShapeRenderer shapeRenderer){
-        shapeRenderer.setColor(2f, 0.8f, 0.6f, 0.4f);
-        shapeRenderer.rect(position.x, position.y, width, length);
-    }
-
-    public void render(ShapeRenderer shaperenderer) {
-
-    }
-}
-
-
->>>>>>> 14a32b6e44e77ede470b1b3f394bf224a4bc0561

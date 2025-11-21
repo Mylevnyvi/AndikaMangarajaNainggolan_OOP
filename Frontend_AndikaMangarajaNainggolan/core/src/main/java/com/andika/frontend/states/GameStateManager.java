cf891816edbc2380f4101a42a95e7404cf619767
aalalain <ashgreninja270@gmail.com>
2025-11-21 11:00:08 +0700

    Frontend Changes CS 10

---
+++ Frontend_AndikaMangarajaNainggolan/core/src/main/java/com/andika/frontend/states/GameStateManager.java
@@ -0,0 +1,49 @@
+package com.andika.frontend.states;
+
+
+import com.andika.frontend.states.GameState;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.util.Stack;
+
+public class GameStateManager {
+    private final Stack<GameState> states;
+
+
+
+    public GameStateManager(){
+        this.states = new Stack<>();
+    }
+
+public void push(GameState state){
+        states.push(state);
+}
+
+public void pop() {
+        if (!states.isEmpty()){
+            GameState gameState = states.pop();
+            gameState.dispose();
+        }
+}
+
+public void set(GameState state){
+    if (!states.isEmpty()){
+        pop();
+    }
+    push(state);
+}
+
+public void update(float delta) {
+        if (!states.isEmpty()){
+            states.peek().update(delta);
+        }
+
+    }
+public void render(SpriteBatch batch) {
+    if (!states.isEmpty()){
+        states.peek().render(batch);
+    }
+
+  }
+
+}
